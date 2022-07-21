a73b8cfadd889c0460361829cfe8f9f706bebd1a
Matt Pauls <mattpauls@gmail.com>
2022-07-20 19:44:25 -0700

    cleaning up

--- accounts-and-rostering/uploadprofilepic/uploadprofilepic.py
+++ accounts-and-rostering/uploadprofilepic/uploadprofilepic.py
@@ -1,20 +1,15 @@
-#from Tkinter import Tk
-#from tkFileDialog import askopenfilename
-#from tkFileDialog import askdirectory
 import os
 
 # Location of GAM
-gam = "/Users/mattpauls/bin/gam/gam"
+gam = "/Users/mpauls/bin/gam/gam"
 
 # Ask for directory where we want to upload pictures from
-#Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-#folder = askdirectory()
-folder = "/Users/mattpauls/Desktop/C47 - Intake Photos RAW/email/4"
+folder = "/Users/mpauls/Desktop/C49 - Intake Portraits/email/4"
 
 # Scan through the directory, pick up all images in it, and upload them to Google Apps.
 # Assumes pictures are named with the email address of the student, e.g. doejohn@mygya.com.jpg
 for profilepic in os.listdir(folder):
-    if profilepic.endswith('.jpg'):
+    if profilepic.endswith('.jpg') or profilepic.endswith('.JPG'):
         emailaddr = profilepic[:-4]
         uploadcommand = gam + " user " + emailaddr + " update photo \"" + os.path.realpath(folder) + os.path.sep + profilepic + "\""
         #print uploadcommand
