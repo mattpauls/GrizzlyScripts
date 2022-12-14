211a0bf429fbd8e02928ffcc41a43e206e156027
Matt Pauls <mattpauls@gmail.com>
2022-12-13 16:43:17 -0800

    cleanup and notes

--- accounts-and-rostering/active-directory/ADStudentCleanup.py
+++ accounts-and-rostering/active-directory/ADStudentCleanup.py
@@ -90,6 +90,7 @@ def moveUser(ad, user, newLocation):
         print(f"Moving {user['samAccountName']} to students OU.")
         userNewDn = "OU=students,DC=GYA,Dc=local"
     elif newLocation == "alumni":
+        # TODO possibly create OU if it doesn't exist.
         print(f"Moving {user['samAccountName']} to alumni OU.")
         userNewDn = getAlumniClass(user["samAccountName"])
     elif newLocation == "isp":
@@ -159,121 +160,6 @@ def searchAD(ad) -> dict:
 
     return students
 
-def processStudents():
-    students = filemakerGetAll()
-    ad = bindAD()
-
-    ad_students = searchAD(ad)
-
-    for student in students:
-        print("student is: ", student)
-
-        # Check if student has a username assigned.
-        if student['SchoolUsername']:
-
-            # Check if student exists in Active Directory (search for account)
-            samaccountname = "samaccountname=" + student['SchoolUsername']
-            result = ad.search_s("dc=GYA, dc=local", ldap.SCOPE_SUBTREE, samaccountname)
-            print(result)
-
-            if result is None:
-                print("student not found in AD")
-            # elif len(result) > 1:
-            #     print(len(result))
-            #     print("somehow we found more than one account with the same samaccountname")
-            else:
-                print("student was found in AD")
-                userCurrentDn = result[0][0]
-                print(result[0][1]['cn'][0].decode('UTF-8'))
-                userRdn = "cn=" + result[0][1]['cn'][0].decode('UTF-8')
-                
-                # Process the student
-
-                # Check if the student is assigned to ISP in FileMaker
-                if student["StatusActive"] == "isp":
-                    print("%s, %s is in independent study" % (student['NameLast'], student['NameFirst']))
-                    # Check if student is currently in the ISP OU
-                    if "OU=isp" in userCurrentDn:
-                        print("student is in ISP in AD, no need to move")
-                        # existingAdStudents[student] = {'correct_location': True}
-                    # If student is not in the ISP OU, move to the ISP OU
-                    else:
-                        print("student is not in ISP in AD, need to move student to ISP OU")
-                        
-                        # Move the student to the ISP OU in Active Directory
-                        print("moving student with info: ", userCurrentDn, userRdn, "OU=ISP,DC=GYA,Dc=local")
-                        # ad.rename_s(userCurrentDn, userRdn, "OU=ISP,DC=GYA,Dc=local") # uncomment to move students
-                
-                # Check if the student is an independent study drop
-                elif student["StatusActive"] == "ind st drop":
-                    print("student is an independent study drop")
-                    # Check if student is in correct alumni OU
-                    # If student is not in alumni OU, Move student to correct alumni OU
-                
-                # Check if the student has been dropped from the residential program
-                elif student["StatusActive"] == "No":
-                    print("Student is inactive")
-                    # Disable student, if not already disabled in AD
-                    # use userAccountControl property https://stackoverflow.com/questions/13597345/enable-disable-account-programmatically-using-python-ldap-module
-                
-                # Check if the student is actively in the residential program
-                elif student["StatusActive"] == "Yes" or student["StatusActive"] == "yes":
-                    print("student is active")
-                    # Check if student is disabled, and enable.
-                else:
-                    print("%s StatusActive has not been coded properly" % student['NameLast'])
-
-                
-        # If no username, no use continuing. Can't do much with that.
-        else:
-            print(print(f"Student {student['NameLast']} does not have a username."))
-
-
-        print(student)
-        break
-    # print(existingAdStudents)
-
-    # TODO write logic to check isp and students OUs for student accounts. Build out a list or dictionary with the usernames and TABEIDs found (if any)
-    # Load students from the ISP OU, after we've done all the processing above.
-    existingAdStudents = searchAD(ad)
-
-    """
-    # Loop through found students in the ISP OU
-    for ispstudent in existingAdStudents:
-        print(ispstudent)
-        found = False
-
-        # Loop through the students that we have in FileMaker.
-        for student in students:
-            # print(student["SchoolUsername"])
-            if student["SchoolUsername"] == ispstudent:
-                found = True
-        
-        # If the student in the ISP OU is not found in FileMaker, move them to the correct Alumni OU
-        if not found:
-            print("didn't find student in ISP OU! Need to move them to the correct alumni OU")
-            
-            alumniClass = ispstudent[-2:] # get the class number from the student's username
-            
-            if alumniClass.isdigit(): # Check if what we got was a number, if so continue, otherwise skip
-                samaccountname = "samaccountname=" + ispstudent
-                # Get account object in AD
-                result = ad.search_s("dc=GYA, dc=local", ldap.SCOPE_SUBTREE, samaccountname)
-                print("student was found in AD")
-                userCurrentDn = result[0][0]
-                print(result[0][1]['cn'][0].decode('UTF-8'))
-                userRdn = "cn=" + result[0][1]['cn'][0].decode('UTF-8')
-
-                alumniOU = "OU=class" + alumniClass + ",OU=alumni,DC=gya,DC=local"
-
-                print("Moving student %s to %s " % (ispstudent, alumniOU))
-                print(userCurrentDn, userRdn, alumniOU)
-                ad.rename_s(userCurrentDn, userRdn, alumniOU)
-    """
-
-    # Close connection
-    ad.unbind_s()
-
 
 def find_student_in_ad(ad, student, ad_students):
     # print(student)
@@ -304,13 +190,13 @@ def find_student_in_ad(ad, student, ad_students):
     return ad_students
 
 
-def processStudents2():
+def processStudents():
     students = filemakerGetAll()
     ad = bindAD()
 
     ad_students = searchAD(ad)
 
-    school_year_end = 0
+    school_year_end = os.getenv("SCHOOL_YEAR_END")
 
     for student in students:
         # print("student is: ", student)
@@ -321,6 +207,7 @@ def processStudents2():
             ad_students = find_student_in_ad(ad, student, ad_students)
 
         # Check if the student is found in ad_students after processing all students
+        # TODO figure out if there are students in ad_students but not in FileMaker (aka orphaned) and move them to alumni as needed
         if fm_username in ad_students:
             # c.print(student)
             # c.print(ad_students[fm_username])
@@ -357,5 +244,4 @@ def processStudents2():
 
     ad.unbind_s()
 
-processStudents2()
-# searchAD()
+processStudents()
