1993e8053db2647c978d0a69876e127230f60d39
Matt Pauls <mattpauls@gmail.com>
2023-07-24 16:04:48 -0700

    code reformat

--- accounts_and_rostering/uploadprofilepic/uploadprofilepic.py
+++ accounts_and_rostering/uploadprofilepic/uploadprofilepic.py
@@ -4,14 +4,15 @@ import os
 gam = "/Users/mpauls/bin/gam/gam"
 
 # Ask for directory where we want to upload pictures from
-folder = "/Users/mpauls/Desktop/C49 - Intake Portraits/email/4"
+folder = "/Users/mpauls/Desktop/Class 51/Mugs/email/4"
 
 # Scan through the directory, pick up all images in it, and upload them to Google Apps.
 # Assumes pictures are named with the email address of the student, e.g. doejohn@mygya.com.jpg
 for profilepic in os.listdir(folder):
     if profilepic.endswith('.jpg') or profilepic.endswith('.JPG'):
         emailaddr = profilepic[:-4]
-        uploadcommand = gam + " user " + emailaddr + " update photo \"" + os.path.realpath(folder) + os.path.sep + profilepic + "\""
-        #print uploadcommand
+        uploadcommand = gam + " user " + emailaddr + " update photo \"" + \
+            os.path.realpath(folder) + os.path.sep + profilepic + "\""
+        # print uploadcommand
         os.system(uploadcommand)
         print("Uploading profile picture for " + emailaddr)
