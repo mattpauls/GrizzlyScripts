7a65e660b9ae4c8849b9620624e3d75abaf1f7a7
Matt Pauls <mattpauls@gmail.com>
2023-07-24 16:04:01 -0700

    add gam delete groups, reformat code

--- accounts_and_rostering/drop_student/drop_student.py
+++ accounts_and_rostering/drop_student/drop_student.py
@@ -12,8 +12,12 @@ from dotenv import load_dotenv
 load_dotenv()
 console = Console()
 
-# TODO add feature to search Filemaker for student, 
+# Location of GAM
+gam = "/Users/mpauls/bin/gam/gam"
+
+# TODO add feature to search Filemaker for student,
 # return username/plt/group/StatusActive to reference or choose to disable
+
 
 def bindAD():
     Scope = ldap.SCOPE_SUBTREE
@@ -26,31 +30,41 @@ def bindAD():
     # print(result)
     return ad
 
+
 ad = bindAD()
 
-student_username = input("Enter cadet username (not email) to disable:")
+student_username = input("Enter cadet username or email to disable:")
+student_username = student_username.split("@")[0]
 
 samaccountname = "samaccountname=" + student_username
 # Get account object in AD
-result = ad.search_s("dc=GYA, dc=local", ldap.SCOPE_SUBTREE, samaccountname) # TODO move base to .env
+result = ad.search_s("dc=GYA, dc=local", ldap.SCOPE_SUBTREE,
+                     samaccountname)  # TODO move base to .env
 # console.print(result[0][1]["userAccountControl"][0])
 if result[0][0] is not None:
     print("student was found in AD")
-    userAccountControl = result[0][1]["userAccountControl"][0] # returns b'512' for active user
+    # returns b'512' for active user
+    userAccountControl = result[0][1]["userAccountControl"][0]
     print(userAccountControl)
 
     dn = result[0][0]
     print(dn)
 
     # Setup modlist
-    mod = modlist.modifyModlist({'userAccountControl': [b'512']},{'userAccountControl': [b'514']})
+    mod = modlist.modifyModlist({'userAccountControl': [b'512']}, {
+                                'userAccountControl': [b'514']})
 
     # TODO then, modify the userAccountControl to be 514 for disabled, if not already disabled
     # as outlined here https://stackoverflow.com/questions/13597345/enable-disable-account-programmatically-using-python-ldap-module
     # probably use the LDAPObject.modify(dn, modlist) https://www.python-ldap.org/en/python-ldap-3.4.3/reference/ldap.html#ldapobject-classes
-    
+
     # add try: catch: for insufficient permissions
     ad.modify_s(dn, mod)
+
+    # Remove Google Groups
+    remove_groups_cmd = gam + " user " + student_username + " delete groups"
+    # print uploadcommand
+    os.system(remove_groups_cmd)
 
 
 else:
