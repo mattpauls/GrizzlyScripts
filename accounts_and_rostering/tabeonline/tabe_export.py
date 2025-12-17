99b50ebf87179cc9d2407b6de4be1eaff646dbd6
Matt Pauls <mattpauls@gmail.com>
2025-12-16 16:49:00 -0800

    add second function for form 13/14 and modify first to distinguish the two

--- accounts_and_rostering/tabeonline/tabe_export.py
+++ accounts_and_rostering/tabeonline/tabe_export.py
@@ -39,11 +39,57 @@ def stu_csv_creator_dict(file_path, header, d):
             writer.writerow(row)
 
 
-def export() -> None:
+def export_1112() -> None:
     """
-    Generates a file students.csv in the output folder, properly formatted for importing to DRC Insight.
+    Generates a file TABE_students_1112.csv in the output folder, properly formatted for importing to DRC Insight.
     """
-    filename = "TABE_students.csv"
+    filename = "TABE_students_1112.csv"
+    header = [
+    "District Code", "School Code", "Student ID", "Student Last Name", 
+    "Student First Name", "Student Middle Initial", "Gender", "Date of Birth", 
+    "Ethnicity", "Race - American Indian or Alaskan Native", "Race - Asian", 
+    "Race - Black or African American", "Race - Native Hawaiian or Other Pacific Islander", 
+    "Race - Caucasian", "Race - Other", "Public Assistance Status", "IEP", "504", 
+    "LEP/ELL", "Program", "Additional Program", "ESL Status", "Labor Force Status", 
+    "HSE Certificate", "HS Diploma", "Classified Accommodation - Reading", 
+    "Classified Accommodation - Mathematics", "Classified Accommodation - Language"
+]
+    file_path = os.path.join(outputfolder, filename)
+
+    students = filemaker_get_records(query=[{'StatusActive': 'Yes'}])
+
+    # Modify dictionary with new keys
+    for s in students:
+        c.print(f"Configuring row for {s['NameLast']}, {s['NameFirst']}")
+        s["District Code"] = district_code
+        s["School Code"] = school_code
+        s["Student ID"] = s["TABEID"]
+        s["Student Last Name"] = classNo + s["Platoon"] + " " + s.pop("NameLast")
+        s["Student First Name"] = s.pop("NameFirst")
+        s["Gender"] = s.pop("Gender")
+        s["Date of Birth"] = s.pop("Birthday")
+        s["Grade"] = s.pop("GradeLevel")
+        s["Student_email"] = s.pop("SchoolEmail")
+
+        if s["SpecialEducationIEP"] in ["Yes", "yes"]:
+            s["IEP"] = "Y"
+        else:
+            s["IEP"] = "N"
+
+        # if s["SpecialEducation504"] in ["Yes", "yes"]:
+        #     s["504"] = "Y"
+        # else:
+        #     s["504"] = "N"
+
+    c.print("\n")
+    c.print(f"Creating file {filename} in {outputfolder}")
+    stu_csv_creator_dict(file_path, header, students)
+
+def export_1314() -> None:
+    """
+    Generates a file TABE_students_1314.csv in the output folder, properly formatted for importing to DRC Insight.
+    """
+    filename = "TABE_students_1314.csv"
     header = [
     "District Code", "School Code", "Student ID", "Student Last Name", 
     "Student First Name", "Student Middle Initial", "Gender", "Date of Birth", 
@@ -90,14 +136,17 @@ def main():
     while (True):
         c.print("\n")
         c.rule(title="TABE Online Export")
-        c.print("1: Generate TABE export file")
-        c.print("2: Exit")
+        c.print("1: Generate TABE FORM 11/12 export file")
+        c.print("2: Generate TABE FORM 13/14 export file")
+        c.print("3: Exit")
 
-        option = Prompt.ask("Enter your choice:", choices=["1", "2"])
+        option = Prompt.ask("Enter your choice:", choices=["1", "2", "3"])
 
         if option == "1":
-            export()
-        elif option == "2":
+            export_1112()
+        if option == "2":
+            export_1314()
+        elif option == "3":
             exit()
 
 
